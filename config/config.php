e8595409b70bbbee0102f49f1def631dc24f1642
unknown <luiz.ferreira203@etec.sp.gov.br>
2025-08-07 11:54:16 -0300

    [feat]conection configuration

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('LOCAL_PATH','http://localhost/Ordernote-SW2-back/');
+define('HOST', 'localhost');
+define('PORT','3306');
+define('DBNAME', 'ordernote-db');
+define('USER', 'root');
+define('PASSWORD', '');
+
+define('CHARSET','utf8');
