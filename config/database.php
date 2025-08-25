e6e56cf615aaf04d95a6d2d9b1deacb5df557bef
unknown <luiz.ferreira203@etec.sp.gov.br>
2025-08-25 09:53:04 -0300

    [feat] Inicio-login

---
+++ config/database.php
@@ -0,0 +1,22 @@
+class Database {
+    private $host = "localhost";
+    private $db_name = "ordernote-db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
