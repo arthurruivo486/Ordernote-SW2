70d0751f7da60e4e93a82ba4250e3bfa3ad616aa
unknown <luiz.ferreira203@etec.sp.gov.br>
2025-08-28 12:49:40 -0300

    [feat] Database.php

--- config/database.php
+++ config/database.php
@@ -7,16 +7,16 @@ class Database {
 
     public function getConnection() {
         $this->conn = null;
-        try {
+        try{
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host=". $this->host . ";dbname=" . $this->db_name,
                 $this->username,
                 $this->password
             );
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
-            echo "Erro de conexão: " . $exception->getMessage();
+        }catch(PDOException $exception){
+          echo "Erro de conexão: " . $exception->getMessage();
         }
         return $this->conn;
     }
+
 }
