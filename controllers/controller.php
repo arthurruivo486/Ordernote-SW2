b26dce541f5c13d5354b88366a75b08b04a617ba
LuizFelipeDSF <160753466+LuizFelipeDSF@users.noreply.github.com>
2025-08-31 17:44:45 -0300

    Delete loginController class

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,15 +1,1 @@
-class loginController {
-    public function index() {
-        require __DIR__ . '/../views/login.php';
-    }
-    public function autenticar(){
-        if($result){
-           session_start();
-           $_SESSION['usuario'] = $result['email'];
-           header("Location: /app/views/home.php");
-           exit;
-        } else {
-           echo "E-mail ou senha inválidos";
-        }
-    }
-}
+
